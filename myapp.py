a7f7fa3a30a7198862481262ff2eb7aab5a0d8f7
akhmadzaqi <kidznet082@gmail.com>
2023-10-27 13:27:08 +0700

    cleancoment

--- myapp.py
+++ myapp.py
@@ -1,41 +1,7 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# import mysql.connector
-#
-# app = Flask(__name__)
-#
-# # MySQL configuration
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'flaskzaq'
-#
-# try:
-#     # Create a MySQL connection
-#     mysql = mysql.connector.connect(
-#         host=app.config['MYSQL_HOST'],
-#         user=app.config['MYSQL_USER'],
-#         password=app.config['MYSQL_PASSWORD'],
-#         database=app.config['MYSQL_DB']
-#     )
-#
-#     # Check if the connection is successful
-#     if mysql.is_connected():
-#         print('Connected to MySQL!')
-# except mysql.connector.Error as e:
-#     print(f"Error: {e}")
-#     # Handle the error (e.g., log it, show an error message, etc.)
-# finally:
-#     # Close the connection if it was opened
-#     if 'mysql' in locals() and mysql.is_connected():
-#         mysql.close()
-#         print('MySQL connection closed.')
-
-
-
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
-app = Flask(__name__, static_url_path='/static')
 
+app = Flask(__name__, static_url_path='/static')
 
 # MySQL configuration
 app.config['MYSQL_HOST'] = 'localhost'
@@ -43,7 +9,9 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flaskzaq'
 
-# Create a MySQL connection
+
+# try:
+# #     # Create a MySQL connection
 mysql = mysql.connector.connect(
     host=app.config['MYSQL_HOST'],
     user=app.config['MYSQL_USER'],
@@ -51,67 +19,79 @@ mysql = mysql.connector.connect(
     database=app.config['MYSQL_DB']
 )
 
+#     # Check if the connection is successful
+#     if mysql.is_connected():
+#         print('Connected to MySQL!')
+# except mysql.connector.Error as e:
+#     print(f"Error: {e}")
+#     # Handle the error (e.g., log it, show an error message, etc.)
+#     raise  # This will re-raise the exception
+# finally:
+#     # Close the connection if it was opened
+#     if 'mysql' in locals() and mysql.is_connected():
+#         mysql.close()
+#         print('MySQL connection closed.')
+
+
 @app.route('/')
 def index():
-    cur = mysql.cursor()
-    cur.execute('SELECT * FROM your_table_name')
-    data = cur.fetchall()
+    with mysql.cursor() as cur:
+        cur.execute('SELECT * FROM your_table_name')
+        data = cur.fetchall()
     return render_template('index.html', data=data)
 
-# # Route to add data
-# # Route to add data
-# @app.route('/add', methods=['POST'])
-# def add():
-#     if request.method == 'POST':
-#         details = request.form
-#         alamat = details.get('alamat')
-#         fakultas = details.get('fakultas')
-#         nomer_hp = details.get('nomer_hp')
 
-#         # Add data to MySQL
-#         cur = mysql.cursor()
-#         cur.execute("INSERT INTO your_table_name(alamat, fakultas, nomer_hp) VALUES (%s, %s, %s)",
-#                     (alamat, fakultas, nomer_hp))
-#         mysql.commit()
-#         cur.close()
-#         return redirect(url_for('index'))
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        details = request.form
+        alamat = details.get('alamat')
+        fakultas = details.get('fakultas')
+        nomer_hp = details.get('nomer_hp')
+
+        # Add data to MySQL
+        with mysql.cursor() as cur:
+            cur.execute("INSERT INTO your_table_name(alamat, fakultas, nomer_hp) VALUES (%s, %s, %s)",
+                        (alamat, fakultas, nomer_hp))
+        mysql.commit()
+
+        return redirect(url_for('index'))
 
 
-# # Route to edit data
-# @app.route('/edit/<id>', methods=['POST', 'GET'])
-# def edit(id):
-#     cur = mysql.cursor()
-#     cur.execute("SELECT * FROM your_table_name WHERE id = %s", (id,))
-#     data = cur.fetchall()
-#     cur.close()
-#     return render_template('edit.html', data=data[0])
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def edit(id):
+    with mysql.cursor() as cur:
+        cur.execute("SELECT * FROM your_table_name WHERE id = %s", (id,))
+        data = cur.fetchall()
+    return render_template('edit.html', data=data[0])
 
-# # Route to save data changes
-# @app.route('/update/<id>', methods=['POST'])
-# def update(id):
-#     if request.method == 'POST':
-#         details = request.form
-#         alamat = details['alamat']
-#         fakultas = details['fakultas']
-#         nomer_hp = details['nomer_hp']
-#         # Update data in MySQL
-#         cur = mysql.cursor()
-#         cur.execute("UPDATE your_table_name SET alamat = %s, fakultas = %s, nomer_hp = %s WHERE id = %s",
-#                     (alamat, fakultas, nomer_hp, id))
-#         mysql.commit()
-#         cur.close()
-#         return redirect(url_for('index'))
 
-# # Route to delete data
-# @app.route('/delete/<id>', methods=['GET'])
-# def delete(id):
-#     # Delete data from MySQL
-#     cur = mysql.cursor()
-#     cur.execute("DELETE FROM your_table_name WHERE id = %s", (id))
-#     mysql.commit()
-#     cur.close()
-#     return redirect(url_for('index'))
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    if request.method == 'POST':
+        details = request.form
+        alamat = details['alamat']
+        fakultas = details['fakultas']
+        nomer_hp = details['nomer_hp']
+
+        # Update data in MySQL
+        with mysql.cursor() as cur:
+            cur.execute("UPDATE your_table_name SET alamat = %s, fakultas = %s, nomer_hp = %s WHERE id = %s",
+                        (alamat, fakultas, nomer_hp, id))
+        mysql.commit()
+
+        return redirect(url_for('index'))
+
+
+@app.route('/delete/<id>', methods=['GET'])
+def delete(id):
+    # Delete data from MySQL
+    with mysql.cursor() as cur:
+        cur.execute("DELETE FROM your_table_name WHERE id = %s", (id,))
+    mysql.commit()
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
